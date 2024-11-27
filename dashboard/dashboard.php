159dc1c6c1a2686a09862df009e11a9d693847f5
nuu <wisnu@gmail.com>
2024-11-27 16:57:53 +0700

    Co-authored-by: Plooman <Plooman@users.noreply.github.com> Co-authored-by: Royhan <rYmS26@users.noreply.github.com>

---
+++ dashboard/dashboard.php
@@ -0,0 +1,47 @@
+<?php
+include '../backend/koneksi/koneksi.php';
+session_start();
+
+if (!isset($_SESSION['email'])) {
+    header('Location: ../login_form.php');
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+    <link rel="stylesheet" href="../style/sidebar.css">
+</head>
+
+<body id="body-pd">
+    <header class="header sisi" id="header">
+        <div class="header_toggle"> <i class='bx bx-menu' id="header-toggle"></i> </div>
+        <div class="header_img"> <img src="https://i.imgur.com/hczKIze.jpg" alt=""> </div>
+    </header>
+    <div class="l-navbar sisi" id="nav-bar">
+        <nav class="nav">
+            <?php include 'sidebar/sidebar.php' ?>
+        </nav>
+    </div>
+    <!--Container Main start-->
+    <div class="height-100 bg-light">
+        <div id="dashboard">
+            <h1>Dashboard</h1>
+        </div>
+    </div>
+    <!--Container Main end-->
+
+    <!-- <script src="../js/sidebar.js"> -->
+
+    </script>
+</body>
+
+</html>
